2e8f2a7f50977114e30ff6f3e7cde8ed3bc1c5f3
Dark Waves Tech <89140466+darkwaves-ofc@users.noreply.github.com>
2024-01-02 12:02:43 +0530

    Update App.ts

--- src/structures/App.ts
+++ src/structures/App.ts
@@ -1,20 +1,21 @@
-import * as express from "express";
-import * as config from "../config";
-import * as cookieParser from "cookie-parser";
-import * as mongoose from "mongoose";
-import * as bodyParser from "body-parser";
-import * as amqp from "amqplib";
-import * as jwt from "jsonwebtoken";
-import * as userDataDb from "../schema/userData";
-import * as cors from "cors";
-import * as CircularJSON from "circular-json";
-import * as dayjs from "dayjs";
-import { EventEmitter } from "ws";
-import { v4 as uuidv4 } from "uuid";
-import { CompressionTypes, Partitioners } from "kafkajs";
-import "ejs";
+import express from "express";
+import config from "../config";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import amqp from "amqplib";
+import jwt from "jsonwebtoken";
+import userDataDb from "../schema/userData";
+import cors from "cors";
+import getUserData from "../utils/getuserData.js";
+import Logger from "../utils/logger";
+// import * as CircularJSON from "circular-json";
+// import * as dayjs from "dayjs";
+// import { EventEmitter } from "ws";
+// import { v4 as uuidv4 } from "uuid";
+// import { CompressionTypes, Partitioners } from "kafkajs";
 
-class App extends express.Application {
+class App {
   private config: any;
   private express: any;
   private routes: Map<string, any>;
@@ -22,17 +23,32 @@ class App extends express.Application {
   private mongoose: any;
   private fileCache: Map<string, any>;
   private linkCache: Map<string, any>;
+  private getUserData: any;
+  private parseURL: any;
+  private jwt: any;
+  private ipport: any;
+  private _errorMiddleware: any;
+  private _requirehandlers: any;
+  private port: any;
+  private links: any;
+  private server: any;
+  private use: any;
+  private listen: any;
+  private set: any;
+  private _router: any;
+  private findUser: any;
+  private connect: any;
 
   constructor() {
-    super();
+    // super();
     this.config = config;
     this.express = express;
     this.routes = new Map();
-    this.logger = require("../utils/logger.js");
+    this.logger = Logger;
     this.mongoose = mongoose;
     this.fileCache = new Map();
     this.linkCache = new Map();
-    this.getUserData = require("../utils/getuserData.js");
+    this.getUserData = getUserData;
     this.mongoose.Promise = global.Promise;
 
     this.mongoose.connection.on("connected", () => {
@@ -61,7 +77,7 @@ class App extends express.Application {
     this._errorMiddleware = async function (
       req: express.Request,
       res: express.Response,
-      next: express.NextFunction,
+      next: express.NextFunction
     ) {
       return res.status(404).json({ error: true, message: "page not found" });
     };
@@ -75,13 +91,13 @@ class App extends express.Application {
           origin: this.config.website.fontendUri,
           methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
           credentials: true,
-        }),
+        })
       );
       this.use(
         async (
           req: express.Request,
           res: express.Response,
-          next: express.NextFunction,
+          next: express.NextFunction
         ) => {
           let checkHost = false;
 
@@ -98,11 +114,11 @@ class App extends express.Application {
 
           res.setHeader(
             "Access-Control-Allow-Headers",
-            "Content-Type, Authorization",
+            "Content-Type, Authorization"
           );
           res.setHeader("Access-Control-Allow-Private-Network", "true");
           next();
-        },
+        }
       );
 
       this.use(cookieParser(this.config.website.secretKey));
@@ -123,7 +139,7 @@ class App extends express.Application {
         await new handler(this).start();
       }
 
-      this._router.stack.forEach((layer, index, layers) => {
+      this._router.stack.forEach((layer: any, index: any, layers: any) => {
         if (layer.handle === this._errorMiddleware) {
           layers.splice(index, 1);
         }
@@ -153,7 +169,8 @@ class App extends express.Application {
             }
 
             let data = await userDataDb.findOne({
-              _id: decoded.uuid,
+              // _id: decoded.uuid,
+              _id: decoded,
             });
 
             if (!data) {
@@ -162,7 +179,7 @@ class App extends express.Application {
             }
 
             const tokenSchema = data.tokens.find(
-              (val: any) => val.token === token,
+              (val: any) => val.token === token
             );
 
             if (!tokenSchema) {
